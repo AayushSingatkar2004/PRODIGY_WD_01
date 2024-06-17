672944574aa098d8b4182b6f468aea2c5f9b7445
Aayush <167849254+AayushSingatkar2004@users.noreply.github.com>
2024-06-17 20:07:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+  
+var countDownDate = new Date("July 5, 2024 15:37:25").getTime();
+ 
+var x = setInterval(function() {
+ 
+  var now = new Date().getTime();
+ 
+  var distance = countDownDate - now;
+ 
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+ 
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+ 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000); 
+
+
+//to open drop down menu
+document.querySelector(".hamburger img").addEventListener("click",()=>{
+    document.querySelector(".navbar").style.top="0"
+})
+
+//to close drop down menu
+document.querySelector(".navbar li:last-child").addEventListener("click",()=>{
+    document.querySelector(".navbar").style.top="-240px"
+})
+
+
